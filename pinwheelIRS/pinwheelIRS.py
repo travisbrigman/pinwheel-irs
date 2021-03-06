6b3076628de9cff894041c9cd9b907137c8e79f9
travisbrigman <travis1000@gmail.com>
2021-03-06 05:37:46 -0600

    built error handling for script

--- pinwheelIRS/pinwheelIRS.py
+++ pinwheelIRS/pinwheelIRS.py
@@ -62,27 +62,38 @@ elif (forms_download, min_year, max_year):
     search_query = string_to_list(forms_download)
 
 # ----------------------------------------------------------------------------
-
+#TODO: Docstrings
+#TODO: Help info for CLI
+#TODO: Readme.txt (maybe .md too?)
+#TODO: test transmitting and setting up
+#TODO: REGEX for Command Line??
 def main():
 
     parsed_html = []
-
     for term in search_query:
         page_index = 0
         page_list = [0, 0, 1]
-        while page_list[1] < page_list[2]:
-            html = fetchHTML(page_index, term)
-            table_body_results, page_numbers = prepHTML(html)
-            page_list = page_numbers
-            parsed_markup = parseHTML(table_body_results)
-            parsed_html.extend(parsed_markup)
-            page_index += 200
-    sorted_data, filtered_list = sort_data(parsed_html, search_query)
-    if search_query:
-        json_conversion = convert_to_json(sorted_data)
-    elif (search_query, min_year, max_year):
-        pdfs = make_pdf_list(filtered_list, min_year, max_year)
-        download_pdfs(pdfs)
+        try:
+            while page_list[1] < page_list[2]:
+                html = fetchHTML(page_index, term)
+                table_body_results, page_numbers = prepHTML(html)
+                page_list = page_numbers
+                parsed_markup = parseHTML(table_body_results)
+                parsed_html.extend(parsed_markup)
+                page_index += 200
+
+            # assert(len(page_list) > 0), "problems"
+
+            sorted_data, filtered_list = sort_data(parsed_html, search_query)
+            if search_query:
+                json_conversion = convert_to_json(sorted_data)
+            elif (search_query, min_year, max_year):
+                pdfs = make_pdf_list(filtered_list, min_year, max_year)
+                download_pdfs(pdfs)
+    
+        except(IndexError):
+            print("couldn't find anything matching the search_query")
+
 
 def string_to_list(form_string):
     if form_string.find(",") > -1:
